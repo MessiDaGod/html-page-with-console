244d4739ba55d79de62eb7d3ffbc987ce6dad51d
joeshakely <joeshakely@gmail.com>
2022-05-19 12:09:59 -0700

    getting better

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const open = require("open");
+var favicon = require('serve-favicon');
+const path = require('path');
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || "0.0.0.0";
+var host = process.env.HOST || "localhost";
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
@@ -9,17 +11,21 @@ const join = require("path");
 
 const app = express();
 
-var dir = "/";
+var dir = "public";
 var page = "index.html";
 
-app.use(express.static(__dirname + dir));
 
+app.use(express.static(path.join(__dirname, dir)));
+app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
 // sendFile will go here
 app.get("/", function (req, res) {
-    res.sendFile(join(dir, page));
+    res.sendFile(path.join(dir, page));
 });
 
+
+// @ts-ignore
 app.listen(port, host, function () {
-    console.log("http://" + host + ":" + dir + page);
+    console.log("http://" + host + ":" + page);
+    // @ts-ignore
     open("http://" + host + ":" + port + "/" + page, { app: "chrome" });
 });
